c5f5d17a64e70057f438c8b0104a2be07af9c34c
Bruno Girome <brunogirome@gmail.com>
2022-05-05 21:35:06 -0300

    Tournament info card

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,150 +1,157 @@
-import { Container, Timer, PlayerTable } from './styles';
+import { Container, Timer, PlayerTable, TourntamentInfo } from './styles';
 
 export function Dashboard() {
   const minutesSeconds = '40:00';
   const milleseconds = ':00';
 
   return (
-    <>
-      <Container>
-        <section>
-          <Timer>
-            <h1>
-              {minutesSeconds} <span>{milleseconds}</span>
-            </h1>
-            <button type="button">Iniciar rodada</button>
-            <div>
-              <p>
-                <span>Tempo</span>
-                <input type="text" placeholder="Tempo em minutos" />
-              </p>
-              <p>
-                <span>Descrição</span>
-                <input type="text" placeholder="Descrição da rodada" />
-              </p>
-            </div>
-          </Timer>
-          <PlayerTable>
-            <table cellSpacing={0} cellPadding={0}>
-              <tr>
-                <th>Player</th>
-                <th>Score</th>
-              </tr>
-              <tr>
-                <td colSpan={2}>
-                  <div>add</div>
-                </td>
-              </tr>
-              <tr>
-                <td>Player name</td>
-                <td>10 V - 10 E - 10 D</td>
-              </tr>
-              <tr>
-                <td>Player name</td>
-                <td>0 V - 0 E - 0 D</td>
-              </tr>
-              <tr>
-                <td>Player name</td>
-                <td>0 V - 0 E - 0 D</td>
-              </tr>
-            </table>
-          </PlayerTable>
-        </section>
-        <section>
+    <Container>
+      <section>
+        <Timer>
+          <h1>
+            {minutesSeconds} <span>{milleseconds}</span>
+          </h1>
+          <button type="button">Iniciar rodada</button>
           <div>
-            <div>
-              Nome do torneio:
-              <input
-                type="text"
-                placeholder="Nome que será exibido na tela de pontuação"
-              />
+            <p>
+              <span>Tempo</span>
+              <input type="text" placeholder="Tempo em minutos" />
+            </p>
+            <p>
+              <span>Descrição</span>
+              <input type="text" placeholder="Descrição da rodada" />
+            </p>
+          </div>
+        </Timer>
+        <PlayerTable>
+          <table cellSpacing={0} cellPadding={0}>
+            <tr>
+              <th>Player</th>
+              <th>Score</th>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <div>add</div>
+              </td>
+            </tr>
+            <tr>
+              <td>Player name</td>
+              <td>10 V - 10 E - 10 D</td>
+            </tr>
+            <tr>
+              <td>Player name</td>
+              <td>0 V - 0 E - 0 D</td>
+            </tr>
+            <tr>
+              <td>Player name</td>
+              <td>0 V - 0 E - 0 D</td>
+            </tr>
+          </table>
+        </PlayerTable>
+      </section>
+      <section>
+        <TourntamentInfo>
+          <div>
+            <span>Nome do torneio:</span>
+            <input
+              type="text"
+              placeholder="Nome que será exibido na tela de pontuação"
+            />
+          </div>
+          <div>
+            <div className="generalInfo">
+              <p>
+                Participantes: <span>13</span>
+              </p>
+              <p>
+                Rodadas: <span>3</span>
+              </p>
+              <p>
+                Início: <span>13:30</span>
+              </p>
+              <div>
+                <button type="button">Finalizar rodada</button>
+              </div>
+            </div>
+            <div className="checkboxRow">
+              <h3>FINAIS</h3>
+              <p>
+                Rodada Final? <input type="checkbox" />
+              </p>
+              <p>
+                Ícone de jogador genérico? <input type="checkbox" />
+              </p>
+              <p>
+                Usar foto dos jogadores? <input type="checkbox" />
+              </p>
+            </div>
+            <div className="checkboxRow">
+              <h3>EXIBIÇÃO</h3>
+              <p>
+                Exibir deck dos jogadores? <input type="checkbox" />
+              </p>
+              <p>
+                Exibir score dos jogadores? <input type="checkbox" />
+              </p>
             </div>
             <div>
-              <div>
-                <p>
-                  Participantes: <span>13</span>
-                </p>
-                <p>
-                  Rodadas: <span>3</span>
-                </p>
-                <p>
-                  Início: <span>13:30</span>
-                </p>
+              <button type="button">Finalizar torneio</button>
+            </div>
+          </div>
+        </TourntamentInfo>
+        <h2>Organização das mesas</h2>
+        <div>
+          <table>
+            <tr>
+              <th> </th>
+              <th>Player 1</th>
+              <th> </th>
+              <th> </th>
+              <th> </th>
+              <th>Player2</th>
+              <th>Tempo Extra</th>
+              <th>
+                <button type="button">Adicionar</button>
+              </th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>
                 <div>
-                  <button type="button">Finalizar rodada</button>
+                  <p>
+                    <input type="text" placeholder="nome do jogador" />
+                  </p>
+                  <p>
+                    <span>Deck</span>
+                    <input type="text" placeholder="Deck utilizado" />
+                  </p>
                 </div>
-              </div>
-              <div>
-                <h3>FINAIS</h3>
-                <p>
-                  Rodada Final? <input type="checkbox" />
-                </p>
-                <p>
-                  Ícone de jogador genérico? <input type="checkbox" />
-                </p>
-                <p>
-                  Usar foto dos jogadores? <input type="checkbox" />
-                </p>
-              </div>
-              <div>
-                <button type="button">Finalizar torneio</button>
-              </div>
-            </div>
-          </div>
-          <h2>Organização das mesas</h2>
-          <div>
-            <table>
-              <tr>
-                <th> </th>
-                <th>Player 1</th>
-                <th> </th>
-                <th> </th>
-                <th> </th>
-                <th>Player2</th>
-                <th>Tempo Extra</th>
-                <th>
-                  <button type="button">Adicionar</button>
-                </th>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>
-                  <div>
-                    <p>
-                      <input type="text" placeholder="nome do jogador" />
-                    </p>
-                    <p>
-                      <span>Deck</span>
-                      <input type="text" placeholder="Deck utilizado" />
-                    </p>
-                  </div>
-                </td>
-                <td>
-                  <input type="text" placeholder="0" />
-                </td>
-                <td>VS.</td>
-                <td>
-                  <input type="text" placeholder="0" />
-                </td>
-                <td>
-                  <div>
-                    <p>
-                      <input type="text" placeholder="nome do jogador" />
-                    </p>
-                    <p>
-                      <span>Deck</span>
-                      <input type="text" placeholder="Deck utilizado" />
-                    </p>
-                  </div>
-                </td>
-                <td>
-                  <input type="text" placeholder="Tempo em minutos" />
-                </td>
-              </tr>
-            </table>
-          </div>
-        </section>
-      </Container>
-    </>
+              </td>
+              <td>
+                <input type="text" placeholder="0" />
+              </td>
+              <td>VS.</td>
+              <td>
+                <input type="text" placeholder="0" />
+              </td>
+              <td>
+                <div>
+                  <p>
+                    <input type="text" placeholder="nome do jogador" />
+                  </p>
+                  <p>
+                    <span>Deck</span>
+                    <input type="text" placeholder="Deck utilizado" />
+                  </p>
+                </div>
+              </td>
+              <td>
+                <input type="text" placeholder="Tempo em minutos" />
+              </td>
+            </tr>
+          </table>
+        </div>
+      </section>
+    </Container>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -197,3 +197,144 @@ export const PlayerTable = styled.div`
     }
   }
 `;
+
+export const TourntamentInfo = styled.div`
+  > div:first-child {
+    padding: 15px 10px;
+
+    display: flex;
+    align-items: center;
+
+    border-bottom: 1px solid rgba(0, 0, 0, 0.15) !important;
+
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 18px;
+
+    input {
+      margin-left: 10px;
+
+      width: 430px;
+      height: 28px;
+      font-size: 12px;
+
+      display: flex;
+      align-items: center;
+
+      padding-left: 5px;
+
+      background: #fdfdfd;
+      border: 1px solid rgba(0, 0, 0, 0.15);
+      border-radius: 3px;
+    }
+  }
+
+  > div + div {
+    button {
+      font-family: 'Source Sans Pro';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 14px;
+      line-height: 16px;
+      color: #fdfdfd;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      text-transform: uppercase;
+
+      background: #333333;
+
+      border: 0;
+      padding: 0 16px;
+      transition: background-color 0.2s;
+
+      border-radius: 5px;
+    }
+
+    color: #595959;
+
+    display: flex;
+
+    font-family: 'Source Sans Pro';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+
+    > div {
+      padding: 15px 10px;
+
+      border-right: 1px solid rgba(0, 0, 0, 0.15) !important;
+
+      h3 {
+        font-family: 'Source Sans Pro';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+
+        color: #404040;
+        margin-bottom: 5px;
+      }
+
+      p {
+        display: flex;
+        justify-content: space-between;
+      }
+
+      p + p {
+        margin-top: 7px;
+      }
+    }
+
+    .generalInfo {
+      p {
+        width: 150px;
+      }
+
+      > div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        margin-top: 15px;
+
+        button {
+          background-color: #664dca;
+
+          width: 150px;
+
+          height: 30px;
+
+          font-size: 12px;
+
+          x &:hover {
+            background: ${shade(0.2, '#664dca')};
+          }
+        }
+      }
+    }
+
+    .checkboxRow {
+      p {
+        width: 180px;
+      }
+    }
+
+    & > div:last-child {
+      width: 100%;
+      display: flex;
+      justify-content: end;
+      align-items: end;
+
+      button {
+        width: 190px;
+        height: 30px;
+      }
+    }
+  }
+`;
